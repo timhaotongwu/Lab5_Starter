a94496f58530ad7664eac0604ea550c724b4bbf9
timhaotongwu <122568570+timhaotongwu@users.noreply.github.com>
2026-05-05 11:19:26 -0700

    complete explore speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,45 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voiceSelect = document.getElementById('voice-select');
+  const textArea = document.querySelector('textarea');
+  const talkButton = document.querySelector('button');
+  const faceImage = document.querySelector('img');
+
+  let voices = [];
+
+  function loadVoices() {
+    voices = speechSynthesis.getVoices();
+
+    voices.forEach(function (voice, index) {
+      const option = document.createElement('option');
+      option.value = index;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  loadVoices();
+
+  speechSynthesis.addEventListener('voiceschanged', function () {
+    voiceSelect.innerHTML = '';
+    loadVoices();
+  });
+
+  talkButton.addEventListener('click', function () {
+    const text = textArea.value;
+    const utterance = new SpeechSynthesisUtterance(text);
+
+    utterance.voice = voices[voiceSelect.value];
+
+    utterance.onstart = function () {
+      faceImage.src = 'assets/images/smiling-open.png';
+    };
+
+    utterance.onend = function () {
+      faceImage.src = 'assets/images/smiling.png';
+    };
+
+    speechSynthesis.speak(utterance);
+  });
 }
